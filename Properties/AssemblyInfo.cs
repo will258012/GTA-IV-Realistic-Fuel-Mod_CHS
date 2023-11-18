fd059e1fcbe59970fe6c80ff72b18244e8319776
will258012 <will258012@gmail.com>
2023-11-18 15:13:59 +0800

    Update Version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RFM - My Own Car")]
-[assembly: AssemblyDescription("RFM - My Own Car")]
+[assembly: AssemblyTitle("Realistic Fuel Mod CHS Version")]
+[assembly: AssemblyDescription("Realistic Fuel Mod CHS Version")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("@lucas-labs")]
-[assembly: AssemblyProduct("RFM - My Own Car")]
+[assembly: AssemblyProduct("Realistic Fuel Mod CHS Version")]
 [assembly: AssemblyCopyright("Public Domain Software")]
 [assembly: AssemblyTrademark("Public Domain Software")]
 [assembly: AssemblyCulture("")]
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.0.*")]
+[assembly: AssemblyVersion("3.5.0.*")]
